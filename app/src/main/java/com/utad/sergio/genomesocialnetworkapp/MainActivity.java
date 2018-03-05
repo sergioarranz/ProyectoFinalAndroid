ca500fb97b022c7299554126fd7f6ce65df45024
Sergio Arranz Sobrino <sergio.arranz@live.u-tad.com>
2018-03-05 22:10:05 +0100

    Root-commit

---
+++ app/src/main/java/com/utad/sergio/genomesocialnetworkapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.utad.sergio.genomesocialnetworkapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
